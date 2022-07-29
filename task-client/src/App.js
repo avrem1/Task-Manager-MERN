491c1c1c8f7de7abf512afcde6d884e809581154
avrem12 <adithyanat@gmail.com>
2022-07-29 11:25:57 +0530

    Add task UI added

--- task-client/src/App.js
+++ task-client/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
     return (
         <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </Router>
+            <ChakraProvider>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </Router>
+            </ChakraProvider>
         </>
     );
 }
--- task-client/src/components/Home.js
+++ task-client/src/components/Home.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { HStack, Input, Box, Button } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 
 function Home() {
-    return <h1>Welcome to Task Manager App</h1>;
+    return (
+        <HStack
+            width="100%"
+            height={"30vh"}
+            align={"center"}
+            justifyContent="center"
+        >
+            <Box
+                marginTop={"80px"}
+                width={"40%"}
+                height="150px"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                gap={2}
+                boxShadow={"2xl"}
+                rounded={"lg"}
+            >
+                <Input
+                    variant="filled"
+                    placeholder="Enter Task"
+                    width={"50%"}
+                />
+                <Button
+                    leftIcon={<AddIcon />}
+                    colorScheme="blue"
+                    variant="solid"
+                >
+                    Tasks
+                </Button>
+            </Box>
+        </HStack>
+    );
 }
 
 export default Home;
